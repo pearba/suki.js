5c493db29876d38cc9de7b550d4dea4662a76f7c
pearba <107358645+pearba@users.noreply.github.com>
2023-10-26 22:22:29 +0800

    3.0 release
    new cross runtime version supports node deno & bun

--- LICENSE.3rd-party.md
+++ LICENSE.3rd-party.md
@@ -18,3 +18,22 @@ https://github.com/less/less.js/blob/master/LICENSE
 ### SJCL by Emily Stark, Mike Hamburg and Dan Boneh at Stanford University
 License: BSD
 https://github.com/bitwiseshiftleft/sjcl/blob/master/LICENSE.txt
+
+### the function of int62 encoding/decoding 
+Origin Copyright (c) 2013 Andrew Nesbitt
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+### string-byte-length
+Copyright © 2022 ehmicky
+License: MIT
+https://github.com/ehmicky/string-byte-length/blob/main/LICENSE
--- suki.js
+++ suki.js
@@ -1,40 +1,44 @@
-//Requires Deno version 1.29+ for Deno runtime
+/**
+ * suki.js NPM pkg for Node, Deno & Bun
+ */
 
-var LIB_FILE = __dirname + '/sjs.gz'
-,	vm = require('vm')
-,	mod = require('module')
-,	zlib = require('zlib')
-,	fs = require('fs')
-,	path = require('path')
-,	include_gz
-,	sjs
+const runtime = 'undefined' !== typeof Deno && 'deno' || 'undefined' !== typeof Bun && 'bun' || 'node'
+,	isDeno = 'deno'===runtime
+,	sjsFile = isDeno ? '' : __dirname + '/cjs-sjs-' + runtime + '.min.js.gz'
+,	vm = require('node:vm')
+,	zlib = require('node:zlib')
+,	fs = require('node:fs')
+,	path = require('node:path')
 ;
 
-include_gz = gz_file => {
-	let exports = {}
-	,	module = {exports}
-	;
-	vm.runInThisContext(
-		mod.wrap(
-			zlib.gunzipSync(
-				fs.readFileSync(
-					path.normalize(gz_file)
-				)
-			).toString()
-		)
-	)(exports, require, module, __filename, __dirname);
-	return module.exports;
-};
+var includeFile = gz_file => {
+		var exports = {}
+		,	module = { exports }
+		;
+		vm.runInThisContext(
+			wrapModule(
+				zlib.gunzipSync(
+					fs.readFileSync(
+						path.normalize(gz_file)
+					)
+				).toString()
+			)
+		)(exports, require, module, __filename, __dirname);
+		return module.exports;
+	}
+,	wrapModule = cjs_str => '(function (exports, require, module, __filename, __dirname) {' + cjs_str + '})'
+,	sjs = isDeno ? require('./cjs-sjs-deno.min.js') : includeFile(sjsFile) //optimization for deno CDN
+;
 
-sjs = module.exports = include_gz(LIB_FILE);
+module.exports = sjs;
 
 sjs.include = (lib_name, lib_file) => {
 	try
 	{
-		sjs[lib_name] = '.gz'===lib_file.slice(-3) ? include_gz(lib_file) : require(lib_file);
+		sjs[lib_name] = '.gz'===lib_file.slice(-3) ? includeFile(lib_file) : require(lib_file);
 	}
 	catch (e)
 	{
-		console.log('include ' + lib_name + 'failed./n/n' + e.stack);
+		console.error('include ' + lib_name + 'failed.', e.stack);
 	}
 };
